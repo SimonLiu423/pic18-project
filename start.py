ec756f87bdd83b018649a17738bfe8ed0535bc9c
SimonLiu <simonliu423@gmail.com>
2025-01-15 11:00:17 +0800

    start.py

--- start.py
+++ start.py
@@ -7,7 +7,31 @@ from mido import MidiFile
 from scipy.interpolate import interp1d
 
 PITCH_PWM_DIFF_THRESHOLD = 100
-SERIAL_PORT = 'COM8'
+SERIAL_PORT = '/dev/cu.usbserial-120'
+
+NOTE_TO_PWM = {
+    45: 500,
+    46: 550,
+    47: 600,
+    48: 700,
+    49: 735,
+    50: 770,
+    51: 785,
+    52: 795,
+    53: 850,
+    54: 867,
+    55: 885,
+    56: 892,
+    57: 910,
+    58: 923,
+    59: 937,
+    60: 940,
+    61: 943,
+    62: 650,
+    63: 700,
+    64: 750,
+    65: 800,
+}
 
 # Configure the serial port
 ser = serial.Serial(
@@ -19,15 +43,19 @@ ser = serial.Serial(
 )
 
 # Load your MIDI file
-midi_file = MidiFile('twinkle_twinkle.mid')
+midi_file = MidiFile('birthday.mid')
 
 prev_pitch_pwm = None
 
 
 def uart_get():
     ret_str = ''
-    while ser.in_waiting:
-        ret_str += ser.read(1).decode('utf-8')
+    while ser.in_waiting or not ret_str.endswith('<end>'):
+        try:
+            ret_str += ser.read(1).decode('utf-8', errors='replace')
+        except UnicodeDecodeError:
+            ret_str += '<?>'  # Replace invalid bytes with a placeholder
+    ret_str = ret_str.replace('<end>', '')
     return ret_str
 
 
@@ -37,8 +65,8 @@ def uart_send(data: str, debug=False):
 
     print("\033[2m UART sent:", data, "\033[0m")
 
-    if not debug:
-        print("\033[2m UART received:", uart_get(), "\033[0m")
+    # if not debug:
+    print("\033[2m UART received:", uart_get(), "\033[0m")
 
 
 def set_pitch_pwm(new_pwm=None, debug=False):
@@ -174,7 +202,6 @@ def tune(debug=False):
                 print('\n')
                 pwm = set_pitch_pwm(debug=debug)
 
-            tune_result(save_file='pwm_freq_data.png')
         elif tune_mode == 2:  # tune automatically
             print("Tuning automatically...")
             print("Tuning finished")
@@ -184,47 +211,92 @@ def tune(debug=False):
     except KeyboardInterrupt:
         pass
     finally:
+        tune_result(save_file='pwm_freq_data.png')
         print("\n\033[91mExit tuning...\033[0m")
 
 
 def play_midi(debug=False):
+    data = []
+    sum = 0
+    flag = False
     try:
         for i, track in enumerate(midi_file.tracks):
             for message in track:
-                # if message.type in ['note_on', 'note_off']:
+                if message.type in ['note_on', 'note_off']:
+                    print(message)
+                    sum += message.time
 
-                if message.type == 'note_off':
-                    time.sleep(message.time * 0.001)
+                    if message.type == 'note_on':
+                        flag = not flag
+                        if not flag:
+                            continue
+                        data.append(f'{NOTE_TO_PWM[message.note]},{sum}')
+                        sum = 0
 
-                if message.type == 'note_on':
-                    uart_send('pitch ' + str(message.note) + '\r', debug=debug)
+                        # uart_send('pitch ' + str(message.note) + '\r', debug=debug)
 
-                    time.sleep(message.time * 0.001)
-
-                    uart_send('pick\r', debug=debug)
+                        # uart_send(f'pick {message.time*10}\r', debug=debug)
 
                     # notes.append({
-                    # "Track": i,
-                    # "Type": message.type,
-                    # "Note": message.note,
-                    # "Velocity": message.velocity,
-                    # "Time": message.time
+                    #     "Track": i,
+                    #     "Type": message.type,
+                    #     "Note": message.note,
+                    #     "Velocity": message.velocity,
+                    #     "Time": message.time
                     # })
-        # while True:
-        #     data_to_send = str(num) + '\r'
-        #     ser.write(data_to_send.encode('utf-8'))  # Send data
-        #     print("Data sent:", data_to_send)
-        #     num += 1
-        #     time.sleep(0.5)
 
-        # Optional: Wait and read response
-        # time.sleep(1)  # Wait for response (if needed)
-        # if ser.in_waiting:
+        # for note in notes:
+        #     print(note)
+            # while True:
+            #     data_to_send = str(num) + '\r'
+            #     ser.write(data_to_send.encode('utf-8'))  # Send data
+            #     print("Data sent:", data_to_send)
+            #     num += 1
+            #     time.sleep(0.5)
+
+            # Optional: Wait and read response
+            # time.sleep(1)  # Wait for response (if needed)
+            # if ser.in_waiting:
             # response = ser.read(ser.in_waiting).decode('utf-8')  # Read available data
             # print("Received:", response)
 
     except Exception as e:
         print("Error:", e)
+
+    seg_idx = 0
+    step = 3
+    while seg_idx + step < len(data):
+        uart_send('play ' + ' '.join(data[seg_idx:seg_idx+step]) + '<done>\r', debug=debug)
+        # time.sleep(0.005)
+        seg_idx += step
+    uart_send('play ' + ' '.join(data[seg_idx:]) + '<done>\r', debug=debug)
+
+
+def pick_mode(debug=False):
+    try:
+        while True:
+            option = int(input("Enter option: 1)Set degree delta 2)Set base degree 3) Pick: "))
+            if option == 1:
+                degree_delta = int(input("Enter degree delta: "))
+                uart_send('pick set degree delta ' + str(degree_delta) + '\r', debug=debug)
+            elif option == 2:
+                base_degree = int(input("Enter base degree: "))
+                uart_send('pick set base degree ' + str(base_degree) + '\r', debug=debug)
+            elif option == 3:
+                uart_send('pick\r', debug=debug)
+            else:
+                print("Invalid option")
+    except KeyboardInterrupt:
+        print("\n\033[91mExit pick mode...\033[0m")
+
+
+def command_mode(debug=False):
+    try:
+        while True:
+            command = input("Enter command: ")
+            uart_send(command + '\r', debug=debug)
+    except KeyboardInterrupt:
+        print("\n\033[91mExit command mode...\033[0m")
 
 
 if __name__ == "__main__":
@@ -232,7 +304,7 @@ if __name__ == "__main__":
     try:
         # Open the serial port if it's not already open
         if not ser.is_open:
-            ser.port = 'COM8'
+            ser.port = SERIAL_PORT
             ser.open()
         print("Connected to serial port " + ser.port)
     except serial.serialutil.SerialException as e:
@@ -243,9 +315,9 @@ if __name__ == "__main__":
         while True:
             if debug_enable:
                 print("\n[Debug mode]")
-                mode = int(input("Enter mode: 1)Tune 2)Play 3)Tune Result 4)Exit Debug: "))
+                mode = int(input("Enter mode: 1)Tune 2)Play 3)Tune Result 4)Exit Debug 5)Pick: "))
             else:
-                mode = int(input("Enter mode: 1)Tune 2)Play 3)Tune Result 4)Debug: "))
+                mode = int(input("Enter mode: 1)Tune 2)Play 3)Tune Result 4)Debug 5)Pick: "))
 
             if mode == 1:
                 tune(debug=debug_enable)
@@ -258,6 +330,8 @@ if __name__ == "__main__":
                     print("\033[91mSerial port is not open, unable to exit debug mode\033[0m")
                 else:
                     debug_enable = not debug_enable
+            elif mode == 5:
+                pick_mode(debug=debug_enable)
             else:
                 print("Invalid mode")
                 continue
